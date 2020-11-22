3bd66594765f6b646fab941a028421dc19fc78f6
Turcu Alexandru <turcuioan18@stud.ase.ro>
2020-11-22 21:55:57 +0200

    Commit 1

--- main/app.js
+++ main/app.js
@@ -1,5 +1,19 @@
 function addTokens(input, tokens){
-    
+    if(typeof input  === "string")
+    {
+        if(input.length >= 6)
+        {
+            
+        }
+        else
+        {
+            throw new Error("Input should have at least 6 characters")
+        } 
+    }
+    else
+    {
+		throw new TypeError("Invalid input");
+	}
 }
 
 const app = {
