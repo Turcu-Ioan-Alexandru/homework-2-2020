0c9bf293b28a5344e4c92be47b9f973bcfd1382d
Turcu Alexandru <turcuioan18@stud.ase.ro>
2020-11-24 22:33:15 +0200

    Commit 2

--- main/app.js
+++ main/app.js
@@ -3,11 +3,28 @@ function addTokens(input, tokens){
     {
         if(input.length >= 6)
         {
-            
+            for(var token in tokens) 
+            {
+                if(typeof token.tokenName === String)
+                {
+                    if(input.includes("..."))
+                    {
+
+                    }
+                    else
+                    {
+                        return input;
+                    }
+                }
+                else
+                {
+                    throw new Error("Invalid array format");
+                }
+            }           
         }
         else
         {
-            throw new Error("Input should have at least 6 characters")
+            throw new Error("Input should have at least 6 characters");
         } 
     }
     else
